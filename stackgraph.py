5c85a5170d0c52191de6a8e8bda5b99824ca27ca
Asthabura <43913816+Asthabura@users.noreply.github.com>
2020-05-14 00:07:04 +0530

    Python code for plotting stack graph using Matplotlib.
    Author: Asthabura
    Date: 13th May,2020

--- stackgraph.py
+++ stackgraph.py
@@ -6,7 +6,7 @@ student3=[1,1,3,1,4,1,6]
 labels=('Student1','Student2','Student3')
 colors=['red','yellow','green']
 plt.stackplot(days,student1,student2,student3,labels=labels,colors=colors)
-plt.legend(loc='upper left')
+plt.legend(loc='upper left') #loc helps us to decide the place of legend bar consisting of labels.
 plt.title('No. of hours contributed each day for school project')
 plt.tight_layout()
 plt.show()
