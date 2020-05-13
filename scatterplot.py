721bf451cdc925a48b9f07b07093ff431e901d6a
Asthabura <43913816+Asthabura@users.noreply.github.com>
2020-05-14 00:05:45 +0530

    Python code for scatter plot using Matplotlib.
    Author: Asthabura
    Date: 13th May,2020

--- scatterplot.py
+++ scatterplot.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 math_marks=[88,92,80,89,100,80,60,100,80,34]
 science_marks=[35,79,79,48,100,88,32,45,20,30]
 marks_range=[10,20,30,40,50,60,70,80,90,100]
-plt.scatter(marks_range,math_marks,color='red',label='Math marks',edgecolor='black',linewidth=1,alpha=0.75)
+plt.scatter(marks_range,math_marks,color='red',label='Math marks',edgecolor='black',linewidth=1,alpha=0.75) #alpha defines the intensity of color.
 plt.scatter(marks_range,science_marks,color='green',label='Science marks',edgecolor='black',linewidth=1,alpha=0.75)
 plt.legend(loc='upper left')
 plt.xlabel('Marks scored')
