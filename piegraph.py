6c217cea3bbd2d6d1fc8eadf02f59fdbdfdd2cb2
Asthabura <43913816+Asthabura@users.noreply.github.com>
2020-05-14 00:03:58 +0530

    Python code to plot pie chart using Matplotlib.
    Author: Asthabura
    Date: 13th May,2020

--- piegraph.py
+++ piegraph.py
@@ -2,8 +2,9 @@ from matplotlib import pyplot as plt
 slices=[46,27,26,19,17]
 labels=['United States','Great Britain','China','Russia','Germany']
 colors=['red','green','yellow','orange','blue']
-explode=[0.1,0,0,0,0]
+explode=[0.1,0,0,0,0] # separates the piece of the pie from the centre by specified distance.
 plt.pie(slices,labels=labels,colors=colors,shadow=True,wedgeprops={'edgecolor':'black'},explode=explode,autopct='%1.1f%%')
+# shadow makes the pie graph look a bit like 3-d and autopct helps to display the percentage each piece represents.
 plt.title('Max gold medals won')
 plt.tight_layout()
 plt.show()
