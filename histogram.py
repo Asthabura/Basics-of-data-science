b0f78023f0b004c60f2fdd48b25374f1993e0be4
Asthabura <43913816+Asthabura@users.noreply.github.com>
2020-05-13 23:58:19 +0530

    Python code for plotting histogram using Matplotlib.
    Author: Asthabura
    Date: 13th May,2020

--- histogram.py
+++ histogram.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 frequency=[6,15,16,21,18]
 height=[0,10,20,24,30,50]
-plt.hist(height,bins=height,edgecolor='black')
+plt.hist(height,bins=height,edgecolor='black') #Bins helps us to decide the band size and edgecolor helps us to distinguish bands.
 plt.title('Heights of plants growing in a garden')
 plt.xlabel('Height')
 plt.tight_layout()
