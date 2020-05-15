b848b5732793bcf587711a04c9b4abd27dc82b51
Asthabura <43913816+Asthabura@users.noreply.github.com>
2020-05-15 15:25:57 +0530

    Update boxplot3.py
    Author: Asthabura
    Date: 15th May,2020

--- boxplot3.py
+++ boxplot3.py
@@ -3,6 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 df = pd.DataFrame(np.random.randn(15, 2),
                   columns=['C1', 'C2'])
-df['Sections'] = pd.Series(['P', 'Q','P','Q',]) #series helps us to name each column of the boxplot
+df['Sections'] = pd.Series(['P', 'Q','P','Q',]) #Series helps us to name each column of the boxplot
 boxplot = df.boxplot(by='Sections',rot=55,fontsize=10,color='green') #by gives a heading to our boxplot
 plt.show()
