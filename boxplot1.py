518d4979da5c1e2a2697e08de1b39f7837147d4c
Asthabura <43913816+Asthabura@users.noreply.github.com>
2020-05-15 15:24:14 +0530

    Update boxplot1.py
    Author: Asthabura
    Date: 15th May,2020

--- boxplot1.py
+++ boxplot1.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 df = pd.DataFrame(np.random.rand(7,3), columns=['A', 'B', 'C'])
-df.plot.box(grid=True)
+df.plot.box(grid=True)#makes grid appear
 plt.show()
