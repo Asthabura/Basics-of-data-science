c19080050b2f53a98b7f35ee00761789c185778b
Asthabura <43913816+Asthabura@users.noreply.github.com>
2020-05-16 16:40:50 +0530

    Add comments
    Author: Asthabura
    Date: 16th May,2020

--- numpy5.py
+++ numpy5.py
@@ -1,7 +1,7 @@
 import numpy as np
 print("Print all the dates in given month")
 print(np.arange('2020-01','2020-02',dtype='datetime64[D]'))
-print("Count the No. of days ")
+print("Count the no. of days ")
 print((np.datetime64('2020-03-01'))-(np.datetime64('2020-02-05')))
 print("today's date")
 print(np.datetime64('today','D'))
